74979780611da90a46554f6aa2c55f6c245a9107
Karim Siala <karimsiala97@gmail.com>
2024-11-16 15:51:15 +0100

    Enable DefaultRouter in DEBUG mode for URL routing

--- orom_backend/scene_manager/urls.py
+++ orom_backend/scene_manager/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path, include
+from django.conf import settings
 from .views import (
     ObjectViewSet, RobotViewSet, SceneViewSet, MujocoSimulationStart, MujocoSimulationStop
 )
-from rest_framework.routers import SimpleRouter
-router = SimpleRouter()
+
+
+# Use DefaultRouter if DEBUG is True, otherwise use SimpleRouter
+if settings.DEBUG:
+    from rest_framework.routers import DefaultRouter
+    router = DefaultRouter()
+else:
+    from rest_framework.routers import SimpleRouter
+    router = SimpleRouter()
+
 router.register('objects', ObjectViewSet)
 router.register('robots', RobotViewSet)
 router.register('scenes', SceneViewSet)
-
 
 urlpatterns = [
     path('', include(router.urls)),
