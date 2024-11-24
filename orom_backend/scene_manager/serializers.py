459eecc04663042ee4dc82bac07ed96de4b807e1
Yannick Pellenz <yannick.pellenz@rwth-aachen.de>
2024-11-24 10:03:35 +0800

    Adapting serializers to solve reference bug

--- orom_backend/scene_manager/serializers.py
+++ orom_backend/scene_manager/serializers.py
@@ -12,7 +12,12 @@ class SceneSerializer(serializers.ModelSerializer):
 
 class ObjectSerializer(serializers.ModelSerializer):
     # Nested serializer to also get information of reference object
-    object_reference = ReferenceObjectSerializer()
+#     object_reference = ReferenceObjectSerializer()
+    object_reference = serializers.PrimaryKeyRelatedField(
+        queryset=ReferenceObjectSerializer.Meta.model.objects.all(),
+        write_only=True
+    )
+    object_reference_details = ReferenceObjectSerializer(source='object_reference', read_only=True)
 
     class Meta:
         model = Object
@@ -22,7 +27,12 @@ class ObjectSerializer(serializers.ModelSerializer):
 
 class RobotSerializer(serializers.ModelSerializer):
     # Nested serializer to also get information of reference robot
-    robot_reference = ReferenceRobotSerializer()
+#     robot_reference = ReferenceRobotSerializer()
+    robot_reference = serializers.PrimaryKeyRelatedField(
+        queryset=ReferenceRobotSerializer.Meta.model.objects.all(),
+        write_only=True
+    )
+    robot_reference_details = ReferenceRobotSerializer(source='robot_reference', read_only=True)
 
     class Meta:
         model = Robot
