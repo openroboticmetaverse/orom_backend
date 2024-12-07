58ae9227a26b219f6c47d1eb3f0566367d01e319
Karim Siala <karimsiala97@gmail.com>
2024-12-07 13:20:31 +0100

    Change file field in AbstractReference model from FileField to CharField and remove num joints field

--- orom_backend/object_library/models.py
+++ orom_backend/object_library/models.py
@@ -5,7 +5,7 @@ import os
 class AbstractReference(models.Model):
     """Abstract class for common properties and functions"""
     name = models.CharField(max_length=255)
-    file = models.FileField(upload_to='object_files/')
+    file = models.CharField(max_length=255)
     file_type = models.CharField(max_length=255, editable=False)
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -13,14 +13,6 @@ class AbstractReference(models.Model):
 
     class Meta: 
         abstract = True
-
-    def save(self, *args, **kwargs):
-        # Set file_type according to uploaded file
-        if self.file and not self.file_type:
-            _, extension = os.path.splitext(self.file.name)
-            self.file_type = extension.lower().strip('.')
-        super().save(*args, **kwargs)
-
 
 
 class ReferenceObject(AbstractReference):
@@ -40,7 +32,6 @@ class ReferenceRobot(AbstractReference):
     Database of objects the user can place in their scene.
     TODO: Make some objects only available for the user who uploaded it
     """
-    num_joints = models.IntegerField()
 
     def __str__(self):
         return f"ReferenceRobot: {self.name} of type {self.file_type}"
